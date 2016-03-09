79226cb4358ec1b94ddc219a8c03e01cc768a5e1
DmitryEliseev <dima-eliseev10@rambler.ru>
2016-03-09 13:45:33 +0300

    Rolling back MainParams class

---
+++ Sudoku/MainParams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    static class MainParams
+    {
+        static public int id { get; set; }
+        static public string solution { get; set; }
+        static public string current_game { get; set; }
+        static public string name { get; set; }
+        static public string difficulty { get; set; }
+    }
+}
